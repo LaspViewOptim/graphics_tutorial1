95785ff645fcc28cb8707a423648120172dfc3db
LipidL <1449196250@qq.com>
2025-02-27 20:57:26 +0800

    petagon drawn!

--- src/lib.rs
+++ src/lib.rs
@@ -34,9 +34,17 @@ impl Vertex {
 }
 
 const VERTICES:&[Vertex] = &[
-    Vertex { position: [0.0, 0.5 , 0.0], color: [1.0, 0.0, 0.0] },
-    Vertex { position: [-0.5, -0.5, 0.0], color: [0.0, 1.0, 0.0] },
-    Vertex { position: [0.5, -0.5, 0.0], color: [0.0, 0.0, 1.0] },
+    Vertex { position: [-0.0868241, 0.49240386, 0.0], color: [0.5, 0.0, 0.5] }, // A
+    Vertex { position: [-0.49513406, 0.06958647, 0.0], color: [0.5, 0.0, 0.5] }, // B
+    Vertex { position: [-0.21918549, -0.44939706, 0.0], color: [0.5, 0.0, 0.5] }, // C
+    Vertex { position: [0.35966998, -0.3473291, 0.0], color: [0.5, 0.0, 0.5] }, // D
+    Vertex { position: [0.44147372, 0.2347359, 0.0], color: [0.5, 0.0, 0.5] }, // E
+];
+
+const INDICIES: &[u16] = &[
+    0, 1, 4, // ABE
+    1, 2, 4, // BCE
+    2, 3, 4, // CDE
 ];
 
 struct State<'a> {
@@ -53,6 +61,8 @@ struct State<'a> {
     render_pipeline: wgpu::RenderPipeline,
     vertex_buffer: wgpu::Buffer,
     num_verticies: u32,
+    index_buffer: wgpu::Buffer,
+    num_indicies: u32,
 }
 
 impl<'a> State<'a> {
@@ -180,7 +190,7 @@ impl<'a> State<'a> {
             // Useful for optimizing shader compilation on Android
             cache: None,
         });
-        // initialize vertex_buffer
+        // initialize vertex_buffer and index_buffer
         let vertex_buffer = device.create_buffer_init(
             &wgpu::util::BufferInitDescriptor {
                 label: Some("Vertex Buffer"),
@@ -189,6 +199,14 @@ impl<'a> State<'a> {
             }
         );
         let num_verticies = VERTICES.len() as u32;
+        let index_buffer = device.create_buffer_init(
+            &wgpu::util::BufferInitDescriptor {
+                label: Some("Index Buffer"),
+                contents: bytemuck::cast_slice(INDICIES),
+                usage: wgpu::BufferUsages::INDEX,
+            }
+        );
+        let num_indicies = INDICIES.len() as u32;
         Self {
             surface,
             device,
@@ -200,6 +218,8 @@ impl<'a> State<'a> {
             render_pipeline,
             vertex_buffer,
             num_verticies,
+            index_buffer,
+            num_indicies,
         }
     }
 
@@ -268,7 +288,8 @@ impl<'a> State<'a> {
             });
             render_pass.set_pipeline(&self.render_pipeline);
             render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
-            render_pass.draw(0..self.num_verticies, 0..1);
+            render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
+            render_pass.draw_indexed(0..self.num_indicies, 0, 0..1);
         }
 
         self.queue.submit(iter::once(encoder.finish()));

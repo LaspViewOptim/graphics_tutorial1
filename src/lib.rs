f7fc32174643add43f2c3028aaaeb4a6160d4901
LipidL <1449196250@qq.com>
2025-03-03 16:52:35 +0800

    improved readability

--- src/lib.rs
+++ src/lib.rs
@@ -22,7 +22,6 @@ pub struct Vertex {
     pub position: [f32; 3],
     pub tex_coords: [f32; 2],
 }
-
 impl Vertex {
     const ATTRIBS: [wgpu::VertexAttribute; 2] = wgpu::vertex_attr_array![
         0 => Float32x3,
@@ -36,21 +35,6 @@ impl Vertex {
         }
     }
 }
-
-const VERTICES: &[Vertex] = &[
-    // Changed
-    Vertex { position: [-0.0868241, 0.49240386, 0.0], tex_coords: [0.4131759, 0.00759614], }, // A
-    Vertex { position: [-0.49513406, 0.06958647, 0.0], tex_coords: [0.0048659444, 0.43041354], }, // B
-    Vertex { position: [-0.21918549, -0.44939706, 0.0], tex_coords: [0.28081453, 0.949397], }, // C
-    Vertex { position: [0.35966998, -0.3473291, 0.0], tex_coords: [0.85967, 0.84732914], }, // D
-    Vertex { position: [0.44147372, 0.2347359, 0.0], tex_coords: [0.9414737, 0.2652641], }, // E
-];
-
-const INDICIES: &[u16] = &[
-    0, 1, 4, // ABE
-    1, 2, 4, // BCE
-    2, 3, 4, // CDE
-];
 
 struct Camera {
     eye: cgmath::Point3<f32>, // coordinates of the camera in world space
@@ -111,7 +95,6 @@ struct CameraController {
     is_left_pressed: bool,
     is_right_pressed: bool,
 }
-
 impl CameraController {
     fn new(speed: f32) -> Self {
         Self {
